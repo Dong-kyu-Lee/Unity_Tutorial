2145e8762e1bbb7b608d6db9e96cbf4d60b89436
Dong-kyu-Lee <stvboy00@gmail.com>
2021-07-10 23:46:20 +0900

    until Lesson 1.4

--- Unit_1/Assets/Script/PlayerController.cs
+++ Unit_1/Assets/Script/PlayerController.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    private float speed = 10.0f;
+    private float turnSpeed = 45.0f;
+    private float horizontalInput;
+    private float forwardInput;
+
     void Start()
     {
         
@@ -11,6 +16,9 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
-        transform.Translate(Vector3.forward * Time.deltaTime*20);
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
     }
 }
