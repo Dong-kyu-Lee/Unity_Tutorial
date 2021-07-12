13829b44e20073b857d429fbf04b975da1951383
Dong-kyu-Lee <stvboy00@gmail.com>
2021-07-12 19:49:30 +0900

    Last

--- Unit_1/Assets/Script/FollowPlayer.cs
+++ Unit_1/Assets/Script/FollowPlayer.cs
@@ -6,6 +6,8 @@ public class FollowPlayer : MonoBehaviour
 {
     public GameObject player;
     private Vector3 offset = new Vector3(0, 5, -7);
+    private Vector3 offset2 = new Vector3(0, 2, 1);
+    private Vector3 resetRotation = new Vector3(30, 0, 0);
 
     void Start()
     {
@@ -14,6 +16,15 @@ public class FollowPlayer : MonoBehaviour
 
     void LateUpdate()
     {
-        transform.position = player.transform.position + offset;
+        if (Input.GetKey(KeyCode.Space))
+        {
+            transform.position = player.transform.position + offset2;
+            transform.rotation = player.transform.rotation;
+        }
+        else
+        {
+            transform.rotation = Quaternion.Euler(resetRotation);
+            transform.position = player.transform.position + offset;
+        }
     }
 }
--- Unit_1/Assets/Script/PlayerController1.cs
+++ Unit_1/Assets/Script/PlayerController1.cs
@@ -2,12 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+public class PlayerController1 : MonoBehaviour
 {
     private float speed = 10.0f;
     public float turnSpeed = 45.0f;
-    private float horizontalInput;
+    public float horizontalInput;
     private float forwardInput;
+
+    private float inputZ;
+    private float inputX;
 
     void Start()
     {
@@ -16,6 +19,14 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
+        /*if(Input.GetKey(KeyCode.UpArrow))
+        {
+            inputZ = 1;
+        }
+        else if(Input.GetKey(KeyCode.DownArrow))
+        {
+            inputZ = -1;
+        }*/
         horizontalInput = Input.GetAxis("Horizontal");
         forwardInput = Input.GetAxis("Vertical");
         transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
