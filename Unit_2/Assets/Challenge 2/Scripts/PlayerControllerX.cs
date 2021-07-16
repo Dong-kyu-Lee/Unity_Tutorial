5ca4a5991e0477feb65d0cee50c8feaa1d8276f9
Dong-kyu-Lee <stvboy00@gmail.com>
2021-07-16 12:29:30 +0900

    Add coroutine to delay SendDog()

--- Unit_2/Assets/Challenge 2/Scripts/PlayerControllerX.cs
+++ Unit_2/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -5,14 +5,27 @@ using UnityEngine;
 public class PlayerControllerX : MonoBehaviour
 {
     public GameObject dogPrefab;
+    bool sending;
 
+    private void Awake()
+    {
+        
+    }
     // Update is called once per frame
     void Update()
     {
         // On spacebar press, send dog
-        if (Input.GetKeyDown(KeyCode.Space))
+        if(Input.GetKeyDown(KeyCode.Space) && sending == false)
         {
-            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            StartCoroutine("SendDog");
         }
+    }
+    
+    IEnumerator SendDog()
+    {
+        sending = true;
+        Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+        yield return new WaitForSeconds(3f);
+        sending = false;
     }
 }
--- Unit_2/Assets/Challenge 2/Scripts/SpawnManagerX.cs
+++ Unit_2/Assets/Challenge 2/Scripts/SpawnManagerX.cs
@@ -11,11 +11,12 @@ public class SpawnManagerX : MonoBehaviour
     private float spawnPosY = 30;
 
     private float startDelay = 1.0f;
-    private float spawnInterval = 4.0f;
+    private int spawnInterval = 0;
 
     // Start is called before the first frame update
     void Start()
     {
+        spawnInterval = Random.Range(3, 5);
         InvokeRepeating("SpawnRandomBall", startDelay, spawnInterval);
     }
 
