a2db3b794bfd15b34b1527514090f1290a4078d4
Dong-kyu-Lee <stvboy00@gmail.com>
2021-07-18 12:00:09 +0900

    Add sound effects

--- Unit_3/Assets/Scripts/PlayerController.cs
+++ Unit_3/Assets/Scripts/PlayerController.cs
@@ -4,9 +4,12 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    Rigidbody playerRb;
+    public AudioClip jumpSound;
+    public AudioClip crashSound;
     public ParticleSystem explosionParticle;
     public ParticleSystem dirtParticle;
+    private AudioSource playerAudio;
+    private Rigidbody playerRb;
     private Animator playerAnim;
     public float jumpForce;
     public float gravityModifier;
@@ -17,6 +20,7 @@ public class PlayerController : MonoBehaviour
     {
         playerRb = GetComponent<Rigidbody>();
         playerAnim = GetComponent<Animator>();
+        playerAudio = GetComponent<AudioSource>();
         Physics.gravity *= gravityModifier;
     }
 
@@ -28,6 +32,7 @@ public class PlayerController : MonoBehaviour
             playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             isOnGround = false;
             playerAnim.SetTrigger("Jump_trig");
+            playerAudio.PlayOneShot(jumpSound, 1.0f);
             dirtParticle.Stop();
         }
     }
@@ -46,6 +51,7 @@ public class PlayerController : MonoBehaviour
             playerAnim.SetBool("Death_b", true);
             playerAnim.SetInteger("DeathType_int", 1);
             dirtParticle.Stop();
+            playerAudio.PlayOneShot(crashSound, 1.0f);
             explosionParticle.Play();
         }
     }
