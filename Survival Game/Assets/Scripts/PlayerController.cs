2d932dba632d855a117665dd77dd9bbaecf9877d
Dong-kyu-Lee <stvboy00@gmail.com>
2021-07-19 12:05:12 +0900

    Make State: Run, Crouch and Jump

--- Survival Game/Assets/Scripts/PlayerController.cs
+++ Survival Game/Assets/Scripts/PlayerController.cs
@@ -8,11 +8,34 @@ public class PlayerController : MonoBehaviour
     private float walkSpeed;
 
     [SerializeField]
+    private float runSpeed;
+    private float applySpeed;
+
+    [SerializeField]
+    private float crouchSpeed;
+
+    [SerializeField]
+    private float jumpForce;
+
+    //상태 변수
+    private bool isRun;
+    private bool isCrouch;
+    private bool isGround;
+
+    [SerializeField]
+    private float crouchPosY; //숙인 값
+    private float originPosY; //원래 값
+    private float applyCrouchPosY;
+
+    // 민감도
+    [SerializeField]
     private float lookSensitivity;
 
     [SerializeField]
     private float cameraRotationLimit;
     private float currentCameraRotationX;
+
+    private CapsuleCollider capsuleCollider;
 
     [SerializeField]
     private Camera theCamera;
@@ -21,15 +44,114 @@ public class PlayerController : MonoBehaviour
 
     void Start()
     {
+        applySpeed = walkSpeed;
         myRigid = GetComponent<Rigidbody>();
+        capsuleCollider = GetComponent<CapsuleCollider>();
+        originPosY = theCamera.transform.position.y;
+        applyCrouchPosY = originPosY;
     }
 
     // Update is called once per frame
     void Update()
     {
+        IsGround();
+        TryJump();
+        TryRun();
+        TryCrouch();
         Move();
         CameraRotation();
         CharacterRotation();
+    }
+
+    //부드러운 앉기 동작
+    IEnumerator CrouchCoroutine()
+    {
+        float _posY = theCamera.transform.localPosition.y;
+        int count = 0;
+        while (_posY != applyCrouchPosY)
+        {
+            count++;
+            //보 뭐(?) 함수 a에서 b까지 c의 비율로 변화
+            _posY = Mathf.Lerp(_posY, applyCrouchPosY, 0.3f);
+            theCamera.transform.localPosition = new Vector3(0, _posY, 0);
+            //15번 실행하면 더 이상 while실행하지 않음 (의미가 없어서)
+            if (count > 15)
+                break;
+            yield return null; // 한 프레임 대기
+        }
+        theCamera.transform.localPosition = new Vector3(0, applyCrouchPosY, 0f);    
+    }
+
+    //앉기 시도
+    void TryCrouch()
+    {
+        if(Input.GetKeyDown(KeyCode.LeftControl))
+        {
+            Crouch();
+        }
+    }
+    //앉기
+    void Crouch()
+    {
+        isCrouch = !isCrouch;
+        if(isCrouch)
+        {
+            applySpeed = crouchSpeed;
+            applyCrouchPosY = crouchPosY;
+        }
+        else
+        {
+            applySpeed = walkSpeed;
+            applyCrouchPosY = originPosY;
+        }
+        StartCoroutine(CrouchCoroutine());
+    }
+
+    //Jump
+    void TryJump()
+    {
+        if(Input.GetKeyDown(KeyCode.Space) && isGround)
+        {
+            Jump();
+        }
+    }
+
+    void Jump()
+    {
+        myRigid.velocity = transform.up * jumpForce;
+        Crouch();
+    }
+
+    void IsGround()
+    {
+        isGround = Physics.Raycast(transform.position, Vector3.down, capsuleCollider.bounds.extents.y + 0.1f);
+    }
+
+    //Run or Walk define
+    private void TryRun()
+    {
+        if(Input.GetKey(KeyCode.LeftShift))
+        {
+            Running();
+        }
+        if(Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            RunningCancel();
+        }
+    }
+
+    void Running()
+    {
+        if(isCrouch)
+            Crouch();
+        isRun = true;
+        applySpeed = runSpeed;
+    }
+
+    void RunningCancel()
+    {
+        isRun = false;
+        applySpeed = walkSpeed;
     }
 
     private void Move()
@@ -39,7 +161,7 @@ public class PlayerController : MonoBehaviour
         Vector3 _moveHorizontal = transform.right * _moveDirX;
         Vector3 _moveVertical = transform.forward * _moveDirZ;
 
-        Vector3 _velocity = (_moveHorizontal + _moveVertical).normalized * walkSpeed;
+        Vector3 _velocity = (_moveHorizontal + _moveVertical).normalized * applySpeed;
         myRigid.MovePosition(transform.position + _velocity * Time.deltaTime);
     }
 
